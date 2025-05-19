3e77a3d96b257f9b75d772f6cba0ee7cf1a2c6f5
AnECanSaiTin <cyyde@163.com>
2025-05-19 21:47:53 +0800

    完善注册器

--- src/main/java/cn/anecansaitin/freecameraapi/common/ModifierRegistry.java
+++ src/main/java/cn/anecansaitin/freecameraapi/common/ModifierRegistry.java
@@ -3,15 +3,17 @@ package cn.anecansaitin.freecameraapi.common;
 import java.util.*;
 
 public class ModifierRegistry {
+    private Map<ModifierPriority,  List<ICameraModifier>> priorityMap;
     private final Map<String, ICameraModifier> modifierMap;
     private final List<ICameraModifier> modifierList;
-    private Map<ModifierPriority,  List<ICameraModifier>> priorityMap;
+    private final List<ICameraModifier> removedList;
     private boolean isFreeze = false;
 
     public ModifierRegistry() {
+        priorityMap = new EnumMap<>(ModifierPriority.class);
         modifierMap = new HashMap<>();
         modifierList = new ArrayList<>();
-        priorityMap = new EnumMap<>(ModifierPriority.class);
+        removedList = new ArrayList<>();
 
         for (ModifierPriority priority : ModifierPriority.values()) {
             priorityMap.put(priority, new ArrayList<>());
@@ -50,5 +52,37 @@ public class ModifierRegistry {
         }
 
         priorityMap = null;
+    }
+
+    public void move(int index, int newIndex) {
+        modifierList.add(newIndex, modifierList.remove(index));
+    }
+
+    public void setOrderById(List<String> ids, List<String> removed) {
+        ArrayList<ICameraModifier> modifiers = new ArrayList<>();
+
+        for (String id : ids) {
+            ICameraModifier modifier = modifierMap.get(id);
+
+            if (modifier == null) {
+                continue;
+            }
+
+            modifiers.add(modifier);
+        }
+
+        for (int i = modifierList.size() - 1; i >= 0; i--) {
+            modifierList.removeAll(modifiers);
+        }
+
+        modifierList.addAll(0, modifiers);
+    }
+
+    public List<ICameraModifier> getModifiers() {
+        return modifierList;
+    }
+
+    public ICameraModifier getActiveModifier() {
+        return null;
     }
 }
