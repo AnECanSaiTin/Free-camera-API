1c7abcb24b0f5d6d771bcfa7bf39c2eb10fe4b59
AnECanSaiTin <cyyde@163.com>
2025-05-29 20:40:18 +0800

    ①尝试添加额外区块渲染

--- src/main/java/cn/anecansaitin/freecameraapi/mixin/ChunkMapMixin.java
+++ src/main/java/cn/anecansaitin/freecameraapi/mixin/ChunkMapMixin.java
@@ -1,17 +1,17 @@
 package cn.anecansaitin.freecameraapi.mixin;
 
-import net.minecraft.server.level.ChunkHolder;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ChunkMap;
-import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChunkMap.class)
 public abstract class ChunkMapMixin {
-    @Inject(method = "onChunkReadyToSend", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;getChunkTrackingView()Lnet/minecraft/server/level/ChunkTrackingView;"))
-    public void onChunkReadyToSend(ChunkHolder p_381769_, LevelChunk p_296003_, CallbackInfo ci) {
+    @ModifyExpressionValue(method = "onChunkReadyToSend", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ChunkTrackingView;contains(Lnet/minecraft/world/level/ChunkPos;)Z"))
+    public boolean onChunkReadyToSend(boolean original, @Local ServerPlayer serverplayer) {
 
+        return original;
     }
 }
